06ca22cfd50ef23ccbee71d4949d191f1a4ab2c3
mumbles1 <justin_turnage@yahoo.com>
2026-09-01 17:35:53 -0500

    Publish Gate Control v1.1.30

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gate-control-v23";
+const CACHE = "gate-control-v24";
 const SHELL = ["/", "/offline.html", "/manifest.webmanifest", "/gate-icon.svg", "/icon-192.png", "/icon-512.png", "/apple-touch-icon.png"];
 
 async function fetchWithTimeout(request, milliseconds) {
